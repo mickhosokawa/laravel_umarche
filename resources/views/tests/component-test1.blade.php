1dc6f9830aad0c67a2fc90a505638e40d92d3125
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-04-29 06:38:57 +0900

    スロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,1 +1,13 @@
+<!-- 
+    {{ $slot }}の部分に "コンポーネントテスト1"
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{ $slot }}
+        </div>
+    </body>
+-->
+
+
+<x-tests.app>
 コンポーネントテスト1
+</x-tests.app>
