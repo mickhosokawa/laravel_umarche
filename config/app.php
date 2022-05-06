93b5eb8a34c0b199748a48d78945215748562c2e
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-05-06 14:27:41 +0900

    multiLogin

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\SampleServiceProvider::class
+        App\Providers\SampleServiceProvider::class //新しく追加
 
     ],
 
