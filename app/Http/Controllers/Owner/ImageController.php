3c687f858c8f60b54ccd6236c420099e704220bb
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-05-20 06:38:50 +0900

    edit, update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -18,12 +18,12 @@ class ImageController extends Controller
         
         // ログインしているユーザ以外が違うショップにログインするのを防ぐ処理
         $this->middleware(function($request, $next){ 
-            $id = $request->route()->parameter('image'); //shopのid取得 
-                if(!is_null($id)){ // null判定 
-                    $imagesOwnerId = Image::findOrFail($id)->owner->id; 
-                    $imageId = (int)imagesOwnerId; // キャスト 文字列→数値に型変換 
-                if($imageId !== Auth::id()){ // 同じでなかったら 
-                    abort(404); // 404画面表示 
+            $id = $request->route()->parameter('image'); 
+            if(!is_null($id)){ 
+            $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId; 
+                if($imageId !== Auth::id()){ 
+                    abort(404);
                 } 
             } 
             return $next($request); 
@@ -83,17 +83,6 @@ class ImageController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
@@ -101,7 +90,9 @@ class ImageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $image = Image::findOrFail($id);
+
+        return view('owner.images.edit', compact('image'));
     }
 
     /**
@@ -113,7 +104,19 @@ class ImageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title'=>'string|max:50'
+        ]);
+
+        $image = Image::findOrFail($id);
+        $image->title = $request->title;
+
+        $image->save();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像タイトルを変更しました。',
+                'status' => 'info']);
     }
 
     /**
--- resources/views/owner/images/create.blade.php
+++ resources/views/owner/images/create.blade.php
@@ -4,31 +4,31 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+  
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{-- ルート情報にショップIDを渡している。また"enctypeは画像を送信するために必要なおまじない --}}
-                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                    <form method='POST' action="{{ route('owner.images.store') }}" enctype="multipart/form-data" >
-                        @csrf
-                        <div class="-m-2">
-                          
-                          <div class="p-2 w-1/2 mx-auto">
-                            <div class="relative">
-                              <label for="image" class="leading-7 text-sm text-gray-600">Image</label>
-                              {{-- file[][image] multiple 複数画像のバリデーション--}}
-                              <input type="file" id="image" name="files[][image]" multiple accept="image/png, image/jpeg, image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                            </div>
-                          </div>
-                          <div class="p-2 w-full flex justify-around mt-4" >
-                            <button type="button" onclick="location.href='{{ route('owner.images.index') }}' "class="text-white bg-gray-400 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">戻る</button>
-                            <button type="submit" class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">登録する</button>
+                  <x-auth-validation-errors class="mb-4" :errors="$errors" />  
+                  <form method="post" action="{{ route('owner.images.store', )}}" enctype="multipart/form-data">
+                      @csrf
+                      <div class="-m-2">
+                       
+                    <div class="p-2 w-1/2 mx-auto">
+                          <div class="relative">
+                            <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
+                            <input type="file" id="image" name="files[][image]" multiple accept=“image/png,image/jpeg,image/jpg” class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                           </div>
                         </div>
+                       
+                            <div class="p-2 w-full flex justify-around mt-4">
+                          <button type="button" onclick="location.href='{{ route('owner.images.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+                          <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>                        
+                        </div>
+                      </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+  </x-app-layout>
