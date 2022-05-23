52518ab131ffd8ec03d4e45bd777530f7ba23603
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-05-23 18:10:07 +0900

    constant

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -189,10 +189,10 @@ class ProductController extends Controller
                         $product->is_selling = $request->is_selling;
                         $product->save();
 
-                    if($request->type === '1'){
+                    if($request->type === \Constant::PRODUCT_LIST['add']){
                         $newQuantity = $request->quantity;
                     }
-                    if($request === '2'){
+                    if($request === \Constant::PRODUCT_LIST['reduce']){
                         $newQuantity = $request->quantity * -1;
                     }
 
